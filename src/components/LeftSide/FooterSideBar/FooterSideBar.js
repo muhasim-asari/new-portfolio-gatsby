16c02a0783bf863071fac85fb4eb5909a33c64dd
muhasim-asari <syababmuslim27@gmail.com>
2022-02-12 01:07:34 +0700

    add right side

--- src/components/LeftSide/FooterSideBar/FooterSideBar.js
+++ src/components/LeftSide/FooterSideBar/FooterSideBar.js
@@ -1,10 +1,16 @@
+import { Link } from 'gatsby';
 import React from 'react';
+import { Headphones, Info, List, UserCheck } from 'react-feather';
 
 const FooterSideBar = () => (
   <div className="footerSideBar">
     <div className="sideBarLinks">
       <ul className="mainLinks listUnstyled">
-        <li></li>
+        <li><Link to="" className="toolTipCust"><span className="toolTipItem"><Info size={24} /></span><span className="toolTipContent">About Me</span></Link></li>
+        <li><Link to="" className="toolTipCust"><span className="toolTipItem"><Image size={24} /></span><span className="toolTipContent">My Portfolio</span></Link></li>
+        <li><Link to="" className="toolTipCust"><span className="toolTipItem"><UserCheck size={24} /></span><span className="toolTipContent">Hire Me</span></Link></li>
+        <li><Link to="" className="toolTipCust"><span className="toolTipItem"><List size={24} /></span><span className="toolTipContent">My Resume</span></Link></li>
+        <li><Link to="" className="toolTipCust"><span className="toolTipItem"><Headphones size={24} /></span><span className="toolTipContent">Contact Me</span></Link></li>
       </ul>
     </div>
   </div>
---
+++ src/components/RightSide/RightSide.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const RightSide = () => (
+  <Col md={12} lg={8}>
+    <div className="rightSide">
+      <div className="secWrapper">
+        <div className="allPages"></div>
+      </div>
+    </div>
+  </Col>
+)
+
+export default RightSide;
